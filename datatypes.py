94bc31a7f74ad9863f8ee27528ba9b6e535bd744
devAshokc <93044023+devAshokc@users.noreply.github.com>
2021-10-23 20:02:49 +0530

    Datatypes details

---
+++ datatypes.py
@@ -0,0 +1,42 @@
+a=23
+print(a)
+
+a=24
+print (int(a) + 1)
+
+
+a="23"+"1"
+print (a)
+
+a=30
+b=40
+c=35
+d=a+b+c
+print(d)
+
+string="hi from ashok"
+print(string)
+
+ashok='hi from ashok'+'test'
+print(ashok)
+
+ashok="hi from ashok"+"test"
+print(ashok[:6])
+
+ashok="hi from ashok"
+print(ashok[2:6])
+
+ashok="hi from ashok"
+print(ashok.split(" "))
+
+ashok="hi from ashok"
+print(ashok.find("s"))
+
+ashok="hi from ashok"
+print(ashok.replace('hi','hello'))
+
+'''ashok="hi from ashok"
+print(ashok.lenth("h"))'''
+
+
+
